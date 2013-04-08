89f580258a0684be4dd3a19a0ed149cd3869631e
gitpablo <monty@monty-ThinkPad-Edge.(none)>
2013-04-08 12:44:30 +0200

    new file:   css/jquerymobile.css 	new file:   css/styles.css 	new file:   database.sql 	new file:   img/farola.svg 	new file:   index.html 	new file:   js/cordova-2.3.0.js 	new file:   js/cordova-2.5.0.js 	new file:   js/html5sql.js 	new file:   js/jquery.js 	new file:   js/jquerymobile.js

---
+++ database.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS farola;
+CREATE TABLE IF NOT EXISTS farola (id, latitud, longitud);
+INSERT INTO farola (id,latitud, longitud) VALUES(0, 41.39991, 2.171942);
+INSERT INTO farola (id,latitud, longitud) VALUES(0, 41.39991, 2.171942);
+INSERT INTO farola (id,latitud, longitud) VALUES(0, 41.39991, 2.171942);
+INSERT INTO farola (id,latitud, longitud) VALUES(0, 41.39991, 2.171942);
