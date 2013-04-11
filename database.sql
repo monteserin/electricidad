4ebbaab62627c29d5e8a9a332ea9df6801d28405
gitpablo <monty@monty-ThinkPad-Edge.(none)>
2013-04-11 10:43:04 +0200

    modified:   README.md 	modified:   css/images/ajax-loader.gif 	modified:   css/images/icons-18-black.png 	modified:   css/images/icons-18-white.png 	modified:   css/images/icons-36-black.png 	modified:   css/images/icons-36-white.png 	modified:   database.sql 	modified:   img/farola.png 	modified:   img/needle_left_yellow_2_24.png 	modified:   index.html 	modified:   js/cordova-2.3.0.js 	modified:   js/html5sql.js 	modified:   main.html

--- database.sql
+++ database.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS farola;
 CREATE TABLE IF NOT EXISTS farola (id, latitud, longitud);
 INSERT INTO farola (id,latitud, longitud) VALUES(0, 41.39991, 2.171942);
-INSERT INTO farola (id,latitud, longitud) VALUES(0, 41.39991, 2.171942);
-INSERT INTO farola (id,latitud, longitud) VALUES(0, 41.39991, 2.171942);
-INSERT INTO farola (id,latitud, longitud) VALUES(0, 41.39991, 2.171942);
+INSERT INTO farola (id,latitud, longitud) VALUES(0, 37.450332, -122.089033);
+INSERT INTO farola (id,latitud, longitud) VALUES(0, 41.38737, 2.175236);
+INSERT INTO farola (id,latitud, longitud) VALUES(0, 41.395612, 2.142963);
+INSERT INTO farola (id,latitud, longitud) VALUES(0, 41.395612, 2.142963);
+INSERT INTO farola (id,latitud, longitud) VALUES(0, 41.369546, 2.134606);
+INSERT INTO farola (id,latitud, longitud) VALUES(0, 41.369756, 2.130378);
